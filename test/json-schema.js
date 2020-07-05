77bfe9d15df2e1b8456382ce32f06fb250d0f5be
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-07-05 04:11:32 +0300

    pointer: use correct base path in visit() resolution

--- test/json-schema.js
+++ test/json-schema.js
@@ -53,7 +53,6 @@ const unsupported = new Set([
   //  draft2019-09 is not supported yet
   'draft2019-09/unevaluatedProperties.json',
   'draft2019-09/unevaluatedItems.json',
-  'draft2019-09/refRemote.json',
   'draft2019-09/ref.json/ref creates new scope when adjacent to keywords',
 ])
 
